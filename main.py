3ddada5c7d737856cf35e862a546eedf67e753a4
Ratchapol <ratcahpol.c@ku.th>
2022-02-09 00:13:04 +0700

    add command receive thread to receive feedback from the Tello after send command

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 # main file for control the Tello with YOLOv5
 # | IMPORT
+import time
 import cv2
 import numpy as np
 import os
 
 from utils.command import Command
-from utils.videoHandle import main_loop
+from utils.videoHandle import VideoHandle
 # | GLOBAL VARIABLES
 FORMAT = "utf-8"
 IP = ""
@@ -25,5 +26,12 @@ YLE = (255, 255, 0)
 
 if __name__ == "__main__":
     cmd = Command(fmt=FORMAT, tello_ip=TELLO_IP, cmd_port=CMD_PORT, self_ip=IP)
+    vh = VideoHandle(img_port=IMG_PORT, req_fps=FPS)
     cmd.init()
-    main_loop(img_port=IMG_PORT, req_fps=FPS)
+    vh.start_thrd()
+    sent = cmd.send(msg="battery?")
+    while vh.run:
+        if vh.cur_cen is not None:
+            print(vh.cur_cen)
+        time.sleep(1)
+    cmd.term = True
--- utils/command.py
+++ utils/command.py
@@ -1,6 +1,9 @@
 # commanding module
 # | IMPORT
 import socket
+import threading
+import time
+
 from typing import Tuple
 
 # | CLASSES
@@ -14,6 +17,24 @@ class Command:
         self.format = fmt
         self.tello_ip = tello_ip
         self.cmd_port = cmd_port
+        self.run = False
+        self.term = False
+
+    def start(self):
+        thrd = threading.Thread(target=self.recv_thrd)
+        thrd.start()
+        self.run = True
+        self.term = False
+
+    def recv_thrd(self):
+        while not self.term:
+            try:
+                data, server = self.cmd_sock.recvfrom(1518)
+            except BlockingIOError:
+                continue
+            else:
+                print(data.decode(encoding=self.format).strip())
+            time.sleep(1)
 
     def __cmd_sock_setup(self) -> socket.socket:
         soc = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -23,7 +44,7 @@ class Command:
 
     def send(self, msg: str, ip: str = None, port: int = None) -> int:
         if ip is None:
-            ip = self.tellop_ip
+            ip = self.tello_ip
         if port is None:
             port = self.cmd_port
 
@@ -32,6 +53,7 @@ class Command:
         return sent
 
     def init(self) -> Tuple[int, int]:
+        self.start()
         tmp1 = self.send(msg="command", ip=self.tello_ip, port=self.cmd_port)
         tmp2 = self.send(msg="streamon", ip=self.tello_ip, port=self.cmd_port)
         return (tmp1, tmp2)
